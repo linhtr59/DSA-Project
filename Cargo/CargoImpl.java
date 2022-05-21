105e538c9a2b833010e0d252206bb182ec401be3
linhtr59 <103659319+linhtr59@users.noreply.github.com>
2022-05-22 00:28:10 +0800

    Update CargoImpl.java

--- Cargo/CargoImpl.java
+++ Cargo/CargoImpl.java
@@ -1,23 +1,54 @@
 // Full Name (StudentNum), Full Name (StudentNum)
 //Linh Trinh (23234683), Thai Hoang Long Nguyen(23147438)
-/**
+/*
  * An implementation of the Cargo problem from the 2022 CITS2200 Project
  */
+
 public class CargoImpl implements Cargo {
   /**
    * {@inheritdoc}
    */
-  private int[] mass;
   public int[] departureMasses(int stops, Query[] queries) {
     // TODO: Implement your solution
-    for (int i = 0; i < queries.length; i++) {
-    	Query q = queries[i];
-    	int m;
-        if (q.collect < q.deliver) {
-        		m += q.cargoMass;
-        			mass[i] = m;
-        		}
-    }
-    return mass;
+      int [] mass = new int[queries.length];
+      fenwicktree fwt = new fenwicktree(stops);   
+      for (int i =0; i < queries.length; i ++) {
+          if (queries[i].collect < queries[i].deliver) {
+              fwt.update(queries[i].collect, queries[i].cargoMass);
+              mass[i] = fwt.getSum(queries[i].collect);
+            }
+          else if (queries[i].collect == queries[i].deliver){
+             mass[i] = fwt.getSum(queries[i].collect);
+            }
+          }
+      return mass;
+  }
+  
+  
+  class fenwicktree{
+      private int[] tree;
+      public fenwicktree( int n) {
+          this.tree = new int[n];
+          }
+
+      public void update(int i, int x) {
+          while (i < tree.length) {
+              tree[i] = tree[i] + x;
+              i += i & -i;
+          }          
+      }
+      
+   
+      public int getSum(int i) { // calculates the prefix sum array from 0 to i
+          int sum = 0;
+          while (i!= 0){
+              sum += tree[i];
+              i -= i & -i;
+          }
+          return sum;
+      }
+      
+      }
+    
 }
-}
+
